3c9af5fb575ceb72888f0ad3c2b4d1bf7cd8ba17
ytokarevskaya <lan2214@yandex.ru>
2020-07-09 09:41:16 +0300

    Проверено

--- task01.js
+++ task01.js
@@ -30,3 +30,21 @@ function onlyNumbers(elem) {
 someArray.forEach((elem) => {
   onlyNumbers(elem);
 });
+
+// Задание выполнено верно, однако в данном случае (т.к. мы точно знаем из задания, что будем работать именно с массивом) было бы лучше написать функцию, которая обрабатывает целый массив, а не каждый элемент. Таким образом она бы вызывалась только один раз, а сейчас функция вызывается на каждом элементе массива. Например можно было бы сделать так:
+
+function printOnlyNumbers(arr) {
+  arr.forEach(elem => {
+    if (typeof elem === "number" && isFinite(elem)) {
+      if (elem == 0) {
+        console.log("It's a 0");
+      } else if (elem % 2 == 0) {
+        console.log("Element " + elem + " is EVEN");
+      } else {
+        console.log("Element " + elem + " is ODD");
+      }
+    }
+  })
+}
+
+printOnlyNumbers(someArray);
--- task04.js
+++ task04.js
@@ -1,7 +1,23 @@
-function rangeFunc(a, b) {
-  return setInterval(function (a, b) {
-    for (i = a; i < b + 1; i++) {
-      console.log(i);
+// function rangeFunc(a, b) {
+//   return setInterval(function (a, b) {
+//     for (i = a; i < b + 1; i++) {
+//       console.log(i);
+//     }
+//   }, 1000, a, b);
+// }
+
+// Функция некорректно решает поставленную задачу. Например, при вызове rangeFunc(1, 3) в консоль должны быть выведены только 3 числа: 1 2 3, каждое число через секунду после предыдущего. Сейчас функция выводит их постоянно, т.к. интервал не останавливается. Верный вариант решения:
+
+function rangeFunc(begin, end) {
+  let current = begin;
+
+  let timerId = setInterval(function() {
+    console.log(current);
+    if (current == end) {
+      clearInterval(timerId);
     }
-  }, 1000, a, b);
+    current++;
+  }, 1000);
 }
+
+rangeFunc(5, 15);
